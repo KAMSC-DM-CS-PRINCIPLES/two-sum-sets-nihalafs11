bb055b14c6ce6df1c7b96001134cb655e9e616c1
Nihal Afsal <nihalafs11@users.noreply.github.com>
2025-09-14 19:49:00 -0400

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -2,7 +2,7 @@ def two_sum_pairs(numbers, target):
     pairs = []
 
     for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
+        for j in range(i, len(numbers)):
             if numbers[i] + numbers[j] == target:
                 pair = {numbers[i], numbers[j]}
                 pairs.append(pair)
